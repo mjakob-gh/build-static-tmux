e82dd0a911cf2439dcaf4dd1c0ba42efa2f138f0
mjakob-gh <19156568+mjakob-gh@users.noreply.github.com>
2020-05-03 14:15:34 +0200

    Update to tmux 3.1b

--- build-static-tmux.sh
+++ build-static-tmux.sh
@@ -27,7 +27,7 @@ TMUX_BIN="tmux.linux-${ARCH}"
 ######################################
 ###### BEGIN VERSION DEFINITION ######
 ######################################
-TMUX_VERSION=3.1a
+TMUX_VERSION=3.1b
 MUSL_VERSION=1.2.0
 NCURSES_VERSION=6.1
 LIBEVENT_VERSION=2.1.11
@@ -369,7 +369,7 @@ printf "Configuring..."
 checkResult $?
 
 # patch file.c
-sed -i 's|#include <sys/queue.h>||g' file.c
+#sed -i 's|#include <sys/queue.h>||g' file.c
 
 printf "Compiling....."
 make >> ${LOG_DIR}/${LOG_FILE} 2>&1
