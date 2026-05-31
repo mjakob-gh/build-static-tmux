06de4b9f8f79818c3c97943a01225f5539c743ae
mjakob-gh <19156568+mjakob-gh@users.noreply.github.com>
2026-05-31 16:11:53 +0200

    Update version numbers for tmux, musl, ncurses, and upx

--- build-static-tmux.sh
+++ build-static-tmux.sh
@@ -15,7 +15,7 @@ COLOR_END="\033[0m"
 PGM="${0##*/}" # Program basename
 
 # Scriptversion
-VERSION=3.5d
+VERSION=3.6b
 
 # How many lines of the error log should be displayed
 LOG_LINES=50
@@ -42,11 +42,11 @@ TMUX_BIN="tmux.${OS}-${ARCH}"
 ######################################
 ###### BEGIN VERSION DEFINITION ######
 ######################################
-TMUX_VERSION=3.6a
-MUSL_VERSION=1.2.5
-NCURSES_VERSION=6.5
+TMUX_VERSION=3.6b
+MUSL_VERSION=1.2.6
+NCURSES_VERSION=6.6
 LIBEVENT_VERSION=2.1.12
-UPX_VERSION=5.0.2
+UPX_VERSION=5.1.1
 ######################################
 ####### END VERSION DEFINITION #######
 ######################################
